f9213f89c0b238c4af4173e275cfb45eda47b3dc
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-24 13:21:22 +0600

    added product image, title, buttons

--- src/components/AllProducts/AllProducts.js
+++ src/components/AllProducts/AllProducts.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import SingleProduct from '../SingleProduct/SingleProduct';
+import "./AllProducts.css";
 
 const AllProducts = () => {
 
@@ -7,12 +9,17 @@ const AllProducts = () => {
     useEffect(()=>{
         fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
-            .then(json=>console.log(json))
+            .then(data=>setProducts(data))
     },[products]);
 
     return (
         <div>
             <h1>All Products</h1>
+            
+            <div className="row container">
+            {products.map((props)=>(<SingleProduct key={props.id} product={props}></SingleProduct>))}
+            </div>
+
         </div>
     );
 };
---
+++ src/components/SingleProduct/SingleProduct.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./SingleProduct.css";
+
+const SingleProduct = (props) => {
+    return (
+        <div className='col-md-4'>
+            <div className="card p-3 border">
+                <img className="w-50 m-auto" src={props.product.image} alt="" />
+            <h1>{props.product.title.slice(0,10)}</h1>
+
+            <div className='d-flex justify-content-around'>
+            <button className='btn btn-success'>Add to Cart</button>
+            <button className='btn btn-danger'>Delete</button>
+            <button className='btn btn-info'>Details</button>
+            </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default SingleProduct;
